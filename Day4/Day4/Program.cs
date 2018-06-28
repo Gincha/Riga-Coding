340dc9b1e7c48a99d4407ee415811a17ca845afb
Gincha <40606700+Gincha@users.noreply.github.com>
2018-06-28 19:18:11 +0300

    Pirmais uzdevums

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzdevums rezultats = new Uzdevums();
+
+            rezultats.Beigas();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day4/Day4/Uzdevums.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    class Uzdevums
+    {
+
+        public void Beigas()
+        {
+            Izvade();
+            Console.WriteLine("Ievadiet pirmo skaitli");
+            String ciparsviens = Console.ReadLine();
+            int a = Convert.ToInt32(ciparsviens);
+
+            Console.WriteLine("Ievadiet otro skaitli");
+            String ciparsDivi = Console.ReadLine();
+            int b = Convert.ToInt32(ciparsDivi);
+
+            int rezultats = Saskaitisana(a,b);
+
+            Console.WriteLine(rezultats);
+
+            Console.ReadLine();
+        }
+
+        private void Izvade()
+        {
+            Console.WriteLine("Sis ir");
+            Console.WriteLine("viegls uzdevums.");
+        }
+
+        private int Saskaitisana(int a, int b)
+        {
+            int result = a+b;
+            return result;
+
+        }
+
+        
+
+    }
+}
