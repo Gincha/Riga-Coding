38bbcd07f0aeb7d20946ff0e1cf1e311c6bac75b
Gincha <40606700+Gincha@users.noreply.github.com>
2018-06-28 20:50:26 +0300

    if un case

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -6,6 +6,7 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            /*
             Uzdevums rezultats = new Uzdevums();
             Zarosanas zari = new Zarosanas();
 
@@ -23,6 +24,66 @@ namespace Day4
                 Console.WriteLine("Izpildas funkcija zari");
                 zari.LielaksVaiMazaks();
             }
+
+            Console.ReadLine();
+            */
+
+
+            /*
+            Console.writeLine("Lai izsauktu 1. funkciju rakstam 1, lai otro - 2");
+            string input = Console.ReadLine();                  // if`s ar stringu nevis ciparu
+
+            if (input == "1")                       // stringu jaliek pedinas
+            {
+                rezultats.Beigas();
+            }
+            else
+            {
+                if (input == "2")
+                {
+                    zari.LielaksVaiMazaks();
+                }
+                else
+                {
+                    Console.WriteLine("Nav pareiza ievade");
+                }
+            }       
+                    
+
+            switch (input)
+            {
+                case "1":
+                    rezultats.Beigas();
+                    break;
+                case "2":
+                    zari.LielaksVaiMazaks();
+                    break;
+                default:                                    // strada ka else -> visi parejie gadijumi kad nav doti
+                    Console.WriteLine("Nav pareiza ievade");
+                    break;                              // katram case beigas jaraksta break OBLIGATI
+            }
+            */
+
+            Zarosanas izvele = new Zarosanas();
+
+            Console.WriteLine("Nospiediet 1 lai palaistu if funkciju, nospiediet 2 lai palaistu ArCase funkciju");
+            String ievada = Console.ReadLine();
+            if (ievada == "1")
+            {
+                izvele.ArIf();
+            }
+            else
+            {
+                if (ievada == "2")
+                {
+                    izvele.ArCase();
+                }
+                else
+                {
+                    Console.WriteLine("Nepareiza ievade");
+                }
+            }
+            
 
             Console.ReadLine();
         }
