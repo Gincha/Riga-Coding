4f674623f7b4f68c8b954b871a7a01771065c54c
Gincha <40606700+Gincha@users.noreply.github.com>
2018-06-29 19:26:51 +0300

    pirmie 4 uzdevumi

---
+++ Day5Uzdevumi/Day5Uzdevumi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Day5Uzdevumi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int vaiBusHotdogs = HotDogs(5);
+            if(vaiBusHotdogs == 1)
+            {
+                Console.WriteLine("voohoo, es sodien tomer edisu");
+            }
+            else
+            {
+                Console.WriteLine("noperc labak roltonu :(");
+            }
+            
+
+            Console.ReadLine();
+            */
+
+            pd1_1 uzdevums = new pd1_1();
+            uzdevums.IzvelamiesFunkciju();
+
+            Console.ReadLine();
+
+        }
+
+
+        /*
+        static int HotDogs(int naudasmaks)
+        { 
+        int result = 0;
+        int hotdogaCena = 3;
+
+        int starpiba = naudasmaks - hotdogaCena;
+
+            if (starpiba >= 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+
+         }
+         */
+
+
+    }
+}
---
+++ Day5Uzdevumi/Day5Uzdevumi/pd1_1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5Uzdevumi
+{
+    class pd1_1
+    {
+        public void Uzdevums1()
+        {
+            PrasamVardu();
+
+        }
+        public void Uzdevums2()
+        {
+            Savienojam();
+        }
+
+        public void Uzdevums3()
+        {
+            Console.WriteLine("Ludzu ievadiet stringu int forma");
+            string strings = Console.ReadLine();
+
+            int cipars = Izvadam(strings);
+            Console.WriteLine(cipars);
+
+
+        }
+
+        private void PrasamVardu()
+        {
+            Console.WriteLine("Ludzu ierakstiet savu vardu un uzvardu");
+            string vardsUzvards = Console.ReadLine();
+
+            Console.WriteLine("Jus sauc " + vardsUzvards);
+
+        }
+
+        private void Savienojam()
+        {
+            Console.WriteLine("ludzu ievadiet pirmo stringu");
+            string pirmais = Console.ReadLine();
+
+            Console.WriteLine("ludzu ievadiet otro stringu");
+            string otrais = Console.ReadLine();
+
+            Console.WriteLine("Savienojot abus stringus sanaks: " + pirmais + otrais);
+
+        }
+
+        private int Izvadam(string viens)
+        {
+
+            int skaitlis = Convert.ToInt32(viens);
+
+            return skaitlis;
+        }
+
+        public void IzvelamiesFunkciju()
+        {
+            Console.WriteLine("Nospiediet atbilstoso skaitli, lai izsauktu funkciju:");
+            Console.WriteLine("1 - Lai izsauktu pirmo funkciju");
+            Console.WriteLine("2 - lai izsauktu otro funkciju");
+            Console.WriteLine("3 - lai izsauktu treso funkciju");
+
+            string ievade = Console.ReadLine();
+
+            if(ievade == "1")
+            {
+                Uzdevums1();
+            }
+            else
+            {
+                if (ievade == "2")
+                {
+                    Uzdevums2();
+                }
+                else
+                {
+                    if (ievade == "3")
+                    {
+                        Uzdevums3();
+                    }else
+                    {
+                        Console.WriteLine("Nepareiza ievade");
+                    }
+                }
+            }
+
+        }
+    }
+}
