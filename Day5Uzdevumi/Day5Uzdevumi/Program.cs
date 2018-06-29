f70baf929b9704ae1ff909c70ad64fc296e82546
Gincha <40606700+Gincha@users.noreply.github.com>
2018-06-29 20:27:06 +0300

    10 uzdevumi

--- Day5Uzdevumi/Day5Uzdevumi/Program.cs
+++ Day5Uzdevumi/Day5Uzdevumi/Program.cs
@@ -21,10 +21,71 @@ namespace Day5Uzdevumi
             Console.ReadLine();
             */
 
+            /*
             pd1_1 uzdevums = new pd1_1();
             uzdevums.IzvelamiesFunkciju();
 
             Console.ReadLine();
+            */
+
+            /*
+            pd1_2 uzdevumi2 = new pd1_2();
+            uzdevumi2.uzdevums5();
+
+            Console.ReadLine();
+            */
+            /*
+            pd1_2 uzdevumi2 = new pd1_2();
+            uzdevumi2.uzdevums6();
+
+            Console.ReadLine();
+            */
+            /*
+            pd1_2 uzdevumi2 = new pd1_2();
+            uzdevumi2.uzdevums9();
+
+            Console.ReadLine();
+            */
+
+            pd1_1 uzdevumi = new pd1_1();
+            pd1_2 uzdevumi2 = new pd1_2();
+
+            Console.WriteLine("Ludzu ievadiet kuru uzdevumu izpildit");
+            string pirmais = Console.ReadLine();
+
+            switch (pirmais)
+            {
+                case "1":
+                    uzdevumi.Uzdevums1();
+                    break;
+                case "2":
+                    uzdevumi.Uzdevums2();
+                    break;
+                case "3":
+                    uzdevumi.Uzdevums3();
+                    break;
+                case "4":
+                    uzdevumi.IzvelamiesFunkciju();
+                    break;
+                case "5":
+                    uzdevumi2.uzdevums5();
+                    break;
+                case "6":
+                    uzdevumi2.uzdevums6();
+                    break;
+                case "7":
+                    uzdevumi2.uzdevums7();
+                    break;
+                case "8":
+                    uzdevumi2.uzdevums8();
+                    break;
+                case "9":
+                    uzdevumi2.uzdevums9();
+                    break;
+                default:
+                    Console.WriteLine("Ludzu ievadiet pareizu uzdevuma nummuru");
+                    break;
+            }
 
         }
 
