28c96ac800680e1fe6f75a6ddc32513ec058371f
Gincha <40606700+Gincha@users.noreply.github.com>
2018-07-02 18:54:58 +0300

    atkartosanas uzd

---
+++ Day6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day_6
+{
+    class Day6
+    {
+        static void Main(string[] args)
+        {
+            Repeat klase = new Repeat();
+
+            klase.Izvade();
+
+            //izsaukt funkciju no otras klases
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeat klase = new Repeat();
+
+            klase.Izvade();
+
+            Console.ReadLine();
+        }
+    }
+}
