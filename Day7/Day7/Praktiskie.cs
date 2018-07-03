7ebe53cc74333c971851665bd83b5be4b017ddce
Gincha <40606700+Gincha@users.noreply.github.com>
2018-07-03 19:00:55 +0300

    Sakuma uzdevumi

---
+++ Day7/Day7/Praktiskie.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7
+{
+    class Praktiskie
+    {
+        public void Pirmais()
+        {
+            //Izvadit summu skaitlu reizinajumam * 2 (skaitli no 1-9)
+            //t.i. (1*2) + (2*2) + (3*2) +...
+
+            int reizin = 0;
+            int atmina = 0;
+            string iekava = "";
+            string kede = "";
+
+            for (int i=1; i<=9; i++)
+            {
+                reizin = i * 2;
+                atmina = atmina + reizin;
+                iekava = Convert.ToString(reizin);
+                kede = kede +iekava + " + ";
+            }
+            Console.WriteLine(kede + " = " + atmina);
+        }
+
+        public void Otrais()
+        {
+            //Likt cilvekam meginat uzminet skaitli. Ja skaitlis sakrit ar 11, tad pareizi
+            //Max meginajumu skaits - 5.
+            //Ja 5 reizes nesanak, tad izvadit pazinojumu.
+
+
+
+            for (int i=1; i<=6; i++)
+            {
+                Console.WriteLine("Uzminiet skaitli");
+                string input = Console.ReadLine();
+                int atbilde = Convert.ToInt32(input);
+
+                if (atbilde == 11)
+                {
+                    Console.WriteLine("Pareizi");
+                    break;
+                }
+                else
+                {
+
+                    Console.WriteLine("Tas meginajums " + i +"/5 ir neveiksmigs :(");
+                    
+                }if (i == 5)
+                {
+                    
+                    Console.WriteLine("Neizdevas, vairak iespeju nebus");
+                    break;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Izveleties uzdevumu- 1 vai 2");
+            string choice = Console.ReadLine();
+            int ch = Convert.ToInt16(choice);
+
+            Praktiskie prak = new Praktiskie(); //Klases objekts
+
+            switch (ch)
+            {
+                case 1:
+                    prak.Pirmais();
+                    break;
+                case 2:
+                    prak.Otrais();
+                    break;
+                default:
+                    Console.WriteLine("Nepareiza ievade!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
