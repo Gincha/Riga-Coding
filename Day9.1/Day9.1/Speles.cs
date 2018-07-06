321e89e789eeb504272f9cb249132b253c8c1739
Gincha <40606700+Gincha@users.noreply.github.com>
2018-07-06 18:24:59 +0300

    Karatava update neatceras rezultatu

--- Day9.1/Day9.1/Speles.cs
+++ Day9.1/Day9.1/Speles.cs
@@ -6,50 +6,62 @@ namespace Day9._1
 {
     class Speles
     {
-        public void Izvade()
+        private void Izvade()
         {
-            string input = "";
-            int i;
 
-            for (i = 1; i < 11; i++)
+            Console.WriteLine("Ievadi vardu");
+            string ievade = Console.ReadLine();
+            char[] vards = ievade.ToCharArray();
+            char[] svitras = ievade.ToCharArray();
+
+            for (int i = 0; i < vards.Length; i++)  //sadala minamo vardu pa burtiem
             {
-                Console.WriteLine("Izvelies karatavu vardu");
-                input = Console.ReadLine();
-                char[] atbilde = input.ToCharArray();
-
-                for (int a = 0; a < atbilde.Length; a++)
-                {
-                    char burts = atbilde[a];
-                    
-                }
-                Console.WriteLine("Sakam");
-
-
-                for (i = 1; i < 11; i++)
-                {
-                    Console.WriteLine("Meginajums Nr." + i + ". Ieraksti burtu");
-                    string chars = Console.ReadLine();
-                    char[] minejums = input.ToCharArray();
-
-                    for (int a = 0; a < minejums.Length; a++)
-                    {
-                        char burti = minejums[a];
-
-                       if (atbilde[a] == minejums[a])
-                        {
-                            Console.WriteLine(minejums[a]);
-                        }
-                    }
-
-
-                    if (i == 11)
-                    {
-                        Console.WriteLine("Meginajumi beigusies");
-                        break;
-                    }
-                }
+                // Get character from array.
+                char burts = vards[i];
             }
 
+            char stripa = '-';
+            //pec copy/paste vairs kludu nerada
+            for (int i = 0; i < vards.Length; i++)   // tads pats garums ka minamajam vardam 
+            {
+                svitras[i] = stripa;
+                char stripBurts = svitras[i];
+
+            }
+
+            Console.WriteLine("Sakam");
+
+            int skaititajs = 0;                  //minesim 10 reizes
+            do
+            {
+                skaititajs++;
+                Console.WriteLine("Ievadi minejumu");
+                string ievade2 = Console.ReadLine();
+                char minejums = Convert.ToChar(ievade2);        //ievada burtu
+
+                string virkne = "";
+                string atmina = "";
+                for (int i = 0; i < vards.Length; i++)      //katram minejumam jaiet cauri visam arrayam ar minamo vardu
+                {
+                    char burts = vards[i];      //bez rindinas neatpazist mainigo if`a
+
+                    if (minejums == burts)          //ja minejums sakrit ar burtu minamaja varda
+                    {
+                        virkne = virkne + vards[i];     //ja sakrit burti tad parada burtu istaja vieta
+                    }
+                    else
+                    {
+                        virkne = virkne + svitras[i];       //ja nesakrit burti tad ieliek svitrinu burta vieta
+                    }
+                }
+
+                atmina = atmina + virkne;
+                Console.WriteLine(atmina);          //parada tikai 1 ciklu
+
+            } while (skaititajs != 10);     //iet cauri kamer nav 10 reizes
+
+            Console.WriteLine("Meginajumu skaits beidzies. Minamais vards bija - " + ievade);
+            Console.ReadLine();
         }
     }
 }
