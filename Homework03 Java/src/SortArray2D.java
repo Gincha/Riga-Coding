640e31253ee11eadff853cc3ac72ecf11a7e0a26
Gincha <40606700+Gincha@users.noreply.github.com>
2018-07-17 11:39:36 +0300

    pabeigts

--- Homework03 Java/src/SortArray2D.java
+++ Homework03 Java/src/SortArray2D.java
@@ -14,19 +14,15 @@ public class SortArray2D {
 		if (ievade < 10 || ievade > 20) {
 			System.out.println("Nepareiza ievade");
 		}
-
-		int randomCipars = 10 + new Random().nextInt(40);
-		
 		int temp;
 		double atmina = 0;
-		double [] videjais = new double[ievade];
+		double[] videjais = new double[ievade];
 		int skaititajs = 0;
-		double temp1;
-		
+		double[] tempAv = new double[videjais.length];
 
 		int[][] masivs2D = new int[ievade][];
 		for (int i = 0; i < ievade; i++) {
-			masivs2D[i] = new int[10 + new Random().nextInt(40)]; // uztaisa random 2. dimensiju
+			masivs2D[i] = new int[10 + new Random().nextInt(41)]; // uztaisa random 2. dimensiju
 
 		}
 
@@ -34,14 +30,14 @@ public class SortArray2D {
 
 			for (int j = 0; j < masivs2D[i].length; j++) {
 
-				masivs2D[i][j] = 100 + new Random().nextInt(899); 	// ieliek ieksha random vertibu visur
+				masivs2D[i][j] = 100 + new Random().nextInt(899); // ieliek ieksha random vertibu visur
 				// System.out.print(i + " " +j + "; "+masivs2D[i][j] + ", ");
 
 			}
 
 		}
 
-		for (int i = 0; i < masivs2D.length; i++) {				 // saliek katru masivu dilstosa seciba
+		for (int i = 0; i < masivs2D.length; i++) { // saliek katru masivu dilstosa seciba
 			for (int j = 0; j < masivs2D[i].length; j++) {
 				for (int k = j + 1; k < masivs2D[i].length; k++) {
 
@@ -52,118 +48,71 @@ public class SortArray2D {
 						masivs2D[i][k] = temp;
 
 					}
-					
 
 				}
-				//System.out.print(masivs2D[i][j] + ", ");
-				
-			}
-			//System.out.println();
-		}
-	
+				// System.out.print(masivs2D[i][j] + ", ");
 
-		for (int i = 0; i < masivs2D.length; i++) {				 	//izrekina videjo katram masivam
+			}
+			// System.out.println();
+		}
+
+		for (int i = 0; i < masivs2D.length; i++) { // izrekina videjo katram masivam
 			for (int j = 0; j < masivs2D[i].length; j++) {
-				 skaititajs++;
-					temp = masivs2D[i][j];
-					atmina = atmina + temp;
-					//System.out.print(atmina + ", ");
-				
+				skaititajs++;
+				temp = masivs2D[i][j];
+				atmina = atmina + temp;
+				// System.out.print(atmina + ", ");
+
 			}
-			videjais[i] = atmina / skaititajs ;
-			System.out.print("Masivam Nr. " + i + " videjais ir " + videjais[i]);
-			System.out.println();
-			atmina=0;
-			skaititajs=0;
-			
+			videjais[i] = atmina / skaititajs;
+			// System.out.print("Masivam Nr. " + i + " videjais ir " + videjais[i]);
+			// System.out.println();
+			atmina = 0;
+			skaititajs = 0;
+
 		}
 
-		/*
-		int[] videjaisInt =new int[videjais.length];
-		
-		for (int i=0; i<videjaisInt.length; i++) {
-			videjaisInt[i] = (int) videjais[i];
-		}
-
-		
-		int[][] atminaArr = new int[masivs2D.length][];	
-		for (int i=0; i<masivs2D.length; i++) {
-			for (int j=0; j<masivs2D[i].length; j++)						//ieliek videjo vertibu ka nulto
-			System.arraycopy(masivs2D, 0, atminaArr, 1, masivs2D[i][j]);
-		}
-
-		
-		for (int i=0; i<atminaArr.length; i++) {		//ieliek videjo vertibu ka nulto
-			for (int j=0; j<1; j++){
-				videjaisInt[i] = atminaArr[i][j];
-			}
-		}
-		
-		
-		
-		for (int i=0; i<atminaArr.length; i++) {
-			for (int j=0; j<atminaArr[i].length; j++){
-				System.out.print(atminaArr[i][j] + ", ");
-				System.out.println();
-			}
-		}
-		*/
-		/*
-		
-
-	    for (int i = 0; i < videjaisInt.length; i++)			//sakartot masivus pec videja NESTRADA!!!
+		for (int i = 0; i < videjais.length; i++)
 
 		{
-			for (int j = i + 1; j < videjaisInt.length; j++)
+			for (int j = i + 1; j < videjais.length; j++)
 
 			{
-				if (videjaisInt[i] > videjaisInt[j])
+				if (videjais[i] > videjais[j])
 
 				{
-					videjaisInt[i] = masivs2D[i];
+					tempAv[i] = videjais[i];
 
+					videjais[i] = videjais[j];
+
+					videjais[j] = tempAv[i];
+
+					int[] tempArr = masivs2D[i];
 					masivs2D[i] = masivs2D[j];
-					
-					masivs2D[j] = atminaArr;
+					masivs2D[j] = tempArr;
+
 				}
 			}
-			
+
 		}
 
+		System.out.println("Lielaka videja vertiba ir masivam Nr. " + (masivs2D.length - 1) + " -> "
+				+ videjais[masivs2D.length - 1]);
 
-		for(int i=0; i<masivs2D.length; i++) {
-			System.out.println(masivs2D[i]);
-		}
-		/*
-		
-		for (int i = 0; i < masivs2D.length; i++) {				 	//izrekina videjo katram masivam
-			for (int j = 0; j < masivs2D[i].length; j++) {
-				 skaititajs++;
-					temp = masivs2D[i][j];
-					atmina = atmina + temp;
-					//System.out.print(atmina + ", ");
-				
+		int lielakaisAv = (masivs2D.length - 1);
+
+		System.out.print("Visas verttibas: ");
+
+		for (int j = 0; j < masivs2D[masivs2D.length - 1].length; j++) {
+
+			if (j == (masivs2D[masivs2D.length - 1].length - 1)) {
+				System.out.print(masivs2D[lielakaisAv][j] + " ");
+
+			} else {
+
+				System.out.print(masivs2D[lielakaisAv][j] + ", ");
 			}
-			videjais[i] = atmina / skaititajs ;
-			System.out.print("Masivam Nr. " + i + " videjais ir " + videjais[i]);
-			System.out.println();
-			atmina=0;
-			skaititajs=0;
-			
 		}
-		
-
-		/*
-		for (int i = 0; i < ievade; i++) {
-
-			for ( j = 0; j < masivs2D[i].length; j++) {
-
-				System.out.print(masivs2D[i][j] + " ");
-
-			}
-			System.out.println();
-		}
-		*/
 
 	}
 
